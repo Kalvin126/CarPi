b65ba392a2c8bc252bc799d5eeffda7be9a703f9
Kalvin Loc <kalvin126@gmail.com>
2019-02-02 20:00:35 -0800

    [ConfigManger] Space by 2

--- View/ConfigManager.rb
+++ View/ConfigManager.rb
@@ -2,20 +2,20 @@ require 'yaml'
 
 class ConfigManager
 
-    def initialize
-        @config = YAML.load_file(File.join(File.dirname(__FILE__), 'config.yml')) 
-    end
+  def initialize
+    @config = YAML.load_file(File.join(File.dirname(__FILE__), 'config.yml'))
+  end
 
-    def port
-        obd_config = @config['obd']
+  def port
+    obd_config = @config['obd']
 
-        return '/dev/ttyUSB0' if obd_config.nil? 
-        
-        port = obd_config['port']
+    return '/dev/ttyUSB0' if obd_config.nil?
 
-        return '/dev/ttyUSB0' if port.nil? 
+    port = obd_config['port']
 
-        return port
-    end
+    return '/dev/ttyUSB0' if port.nil?
+
+    return port
+  end
 
 end
